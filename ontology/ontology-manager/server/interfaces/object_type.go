4803eb8123aa51c6d26f07799291e40293dfd3c7
Yana-tao <taoxiaoyan1016@qq.com>
2026-02-05 09:23:12 +0800

    [fix] 逻辑属性-指标，需返回指标模型所拥有的分析维度

--- ontology/ontology-manager/server/interfaces/object_type.go
+++ ontology/ontology-manager/server/interfaces/object_type.go
@@ -135,13 +135,14 @@ type DataProperty struct {
 }
 
 type LogicProperty struct {
-	Name        string        `json:"name" mapstructure:"name"`
-	DisplayName string        `json:"display_name" mapstructure:"display_name"`
-	Type        string        `json:"type" mapstructure:"type"`
-	Comment     string        `json:"comment" mapstructure:"comment"`
-	Index       bool          `json:"index" mapstructure:"index"`
-	DataSource  *ResourceInfo `json:"data_source" mapstructure:"data_source"`
-	Parameters  []Parameter   `json:"parameters" mapstructure:"parameters"`
+	Name         string        `json:"name" mapstructure:"name"`
+	DisplayName  string        `json:"display_name" mapstructure:"display_name"`
+	Type         string        `json:"type" mapstructure:"type"`
+	Comment      string        `json:"comment" mapstructure:"comment"`
+	Index        bool          `json:"index" mapstructure:"index"`
+	DataSource   *ResourceInfo `json:"data_source" mapstructure:"data_source"`
+	Parameters   []Parameter   `json:"parameters" mapstructure:"parameters"`
+	AnalysisDims []Field       `json:"analysis_dimensions,omitempty"`
 }
 
 type Field struct {
