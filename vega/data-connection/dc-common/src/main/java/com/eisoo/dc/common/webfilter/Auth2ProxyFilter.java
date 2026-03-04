0dda5d3c4260ca915f0926dd85fb5a22b2ac31a0
王美栊（Molen） <molen.wang@aishu.cn>
2026-03-04 11:45:26 +0800

    [fix] vega-gateway-pro/data-connection 支持认证开关，实现与ISF解耦

--- vega/data-connection/dc-common/src/main/java/com/eisoo/dc/common/webfilter/Auth2ProxyFilter.java
+++ vega/data-connection/dc-common/src/main/java/com/eisoo/dc/common/webfilter/Auth2ProxyFilter.java
@@ -1,5 +1,6 @@
 package com.eisoo.dc.common.webfilter;
 
+import com.eisoo.dc.common.auditLog.enums.OperatorType;
 import com.eisoo.dc.common.constant.Detail;
 import com.eisoo.dc.common.constant.Message;
 import com.eisoo.dc.common.exception.enums.ErrorCodeEnum;
@@ -23,9 +24,11 @@ import java.util.*;
 public class Auth2ProxyFilter implements Filter {
 
     private String  url;
+    private boolean authEnabled;
     private Gson gson = new Gson();
-    public Auth2ProxyFilter(String  url) {
+    public Auth2ProxyFilter(String  url, boolean authEnabled) {
         this.url=url;
+        this.authEnabled=authEnabled;
     }
 
     @Override
@@ -39,7 +42,7 @@ public class Auth2ProxyFilter implements Filter {
         String auth2Token = null;
         // uri
         String uri = request.getRequestURI();
-        if (uri.startsWith("/api/data-connection/v1/datasource")
+        if (authEnabled && uri.startsWith("/api/data-connection/v1/datasource")
                 || uri.startsWith("/api/data-connection/v1/metadata")
                 || uri.startsWith("/api/data-connection/v1/gateway")) {
 
