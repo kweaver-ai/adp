f73113b996c4053ba6afbd3b609e6403094b4bb3
emma.zhao <1695599369@qq.com>
2026-03-06 18:01:07 +0800

    [feat]添加衍生视图查询逻辑

---
+++ vega/vega-backend/server/logics/resource_data/logical_view.go
@@ -0,0 +1,73 @@
+package resource_data
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/kweaver-ai/TelemetrySDK-Go/exporter/v2/ar_trace"
+	"github.com/kweaver-ai/kweaver-go-lib/logger"
+	"github.com/kweaver-ai/kweaver-go-lib/rest"
+	"github.com/mitchellh/mapstructure"
+	"go.opentelemetry.io/otel/codes"
+
+	verrors "vega-backend/errors"
+	"vega-backend/interfaces"
+)
+
+func (rds *resourceDataService) QueryLogicalView(ctx context.Context, resource *interfaces.Resource,
+	params *interfaces.ResourceDataQueryParams) ([]map[string]any, int64, error) {
+
+	ctx, span := ar_trace.Tracer.Start(ctx, "Query logical view")
+	defer span.End()
+
+	logger.Debugf("Query logical view, resourceID: %s, params: %v",
+		resource.ID, params)
+
+	// 简单的衍生表查询
+	var sourceNode *interfaces.DataScopeNode
+	for _, logicNode := range resource.LogicDefinition {
+		if logicNode.Type == interfaces.DataScopeNodeType_View {
+			sourceNode = logicNode
+			break
+		}
+	}
+
+	var sourceNodeConfig interfaces.ResourceNodeCfg
+	err := mapstructure.Decode(sourceNode.Config, &sourceNodeConfig)
+	if err != nil {
+		span.SetStatus(codes.Error, "Decode source node config failed")
+		return nil, 0, rest.NewHTTPError(ctx, http.StatusInternalServerError, verrors.VegaBackend_Resource_InternalError).
+			WithErrorDetails(fmt.Sprintf("failed to decode source node config: %v", err))
+	}
+
+	sourceResource, err := rds.rs.GetByID(ctx, sourceNodeConfig.ResourceID)
+	if err != nil {
+		span.SetStatus(codes.Error, "Get source resource failed")
+		return nil, 0, rest.NewHTTPError(ctx, http.StatusInternalServerError, verrors.VegaBackend_Resource_InternalError).
+			WithErrorDetails(fmt.Sprintf("failed to get source resource: %v", err))
+	}
+	if sourceResource == nil {
+		span.SetStatus(codes.Error, "Source resource not found")
+		return nil, 0, rest.NewHTTPError(ctx, http.StatusNotFound, verrors.VegaBackend_Resource_NotFound).
+			WithErrorDetails(fmt.Sprintf("source resource %s not found", sourceNodeConfig.ResourceID))
+	}
+
+	outputFields := make([]string, 0, len(sourceNode.OutputFields))
+	for _, f := range sourceNode.OutputFields {
+		outputFields = append(outputFields, f.Name)
+	}
+
+	newParams := &interfaces.ResourceDataQueryParams{
+		Offset:          params.Offset,
+		Limit:           params.Limit,
+		Sort:            params.Sort,
+		FilterCondition: sourceNodeConfig.Filters,
+		OutputFields:    outputFields,
+		NeedTotal:       params.NeedTotal,
+		Format:          params.Format,
+		Timeout:         params.Timeout,
+	}
+
+	return rds.QueryData(ctx, sourceResource, newParams)
+}
