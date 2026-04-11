33d56ef7309885f1dc0bfa10edba55ae3d71ec2a
“Ken.li” <Ken.li@kweaver.ai>
2026-04-11 14:49:23 +0800

    refactor: 将adp重命名为kweaver
    统一将代码中的adp命名空间修改为kweaver，包括Dockerfile中的用户组、数据库连接配置和消息主题等

--- vega/data-connection/dc-common/src/main/java/com/eisoo/dc/common/msq/Topic.java
+++ vega/data-connection/dc-common/src/main/java/com/eisoo/dc/common/msq/Topic.java
@@ -3,7 +3,7 @@ package com.eisoo.dc.common.msq;
 public enum Topic {
     ISF_AUDIT_LOG_LOG("isf.audit_log.log", "新增审计日志"),
     AUTHORIZATION_RESOURCE_NAME_MODIFY("authorization.resource.name.modify", "修改资源名称"),
-    AF_DATASOURCE_MESSAGE_TOPIC("adp.datasource", "数据源消息");
+    AF_DATASOURCE_MESSAGE_TOPIC("kweaver.datasource", "数据源消息");
 
     private final String topicName;
     private final String description;
