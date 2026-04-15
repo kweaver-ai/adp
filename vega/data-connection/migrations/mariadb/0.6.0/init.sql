78b3f09b6cc0858007289a936a88e8dc59e4c80d
Leopold.yu <Leopold.yu@aishu.cn>
2026-04-15 16:06:15 +0800

    追加etrino模块数据库脚本

--- vega/data-connection/migrations/mariadb/0.6.0/init.sql
+++ vega/data-connection/migrations/mariadb/0.6.0/init.sql
@@ -756,3 +756,154 @@ INSERT INTO t_lineage_graph_info (app_id,graph_id) SELECT '3',0 FROM DUAL WHERE 
 INSERT INTO t_lineage_graph_info (app_id,graph_id) SELECT '4',0 FROM DUAL WHERE NOT EXISTS ( SELECT app_id from t_lineage_graph_info where app_id = 4);
 INSERT INTO t_lineage_graph_info (app_id,graph_id) SELECT '5',0 FROM DUAL WHERE NOT EXISTS ( SELECT app_id from t_lineage_graph_info where app_id = 5);
 INSERT INTO t_lineage_graph_info (app_id,graph_id) SELECT '6',0 FROM DUAL WHERE NOT EXISTS ( SELECT app_id from t_lineage_graph_info where app_id = 6);
+
+
+
+-- 追加etrino模块数据库初始化
+CREATE TABLE IF NOT EXISTS `catalog_rule` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `catalog_name` varchar(200) NOT NULL COMMENT 'catalog名称',
+  `datasource_type` varchar(50) NOT NULL COMMENT '数据源类型',
+  `pushdown_rule` varchar(500) DEFAULT NULL COMMENT '下推规则',
+  `is_enabled` varchar(20) NOT NULL COMMENT '是否启用规则',
+  `create_time` varchar(30) DEFAULT NULL COMMENT '创建时间',
+  `update_time` varchar(30) DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_ctlgs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `catalog_name` varchar(256) NOT NULL,
+  `create_time` bigint(20) NOT NULL,
+  `owner` varchar(767) DEFAULT NULL,
+  `comment` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `catalog_name` (`catalog_name`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_dbs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `catalog_name` varchar(256) NOT NULL,
+  `database_name` varchar(256) NOT NULL,
+  `create_time` bigint(20) NOT NULL,
+  `owner` varchar(767) DEFAULT NULL,
+  `comment` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `catalog_name` (`catalog_name`,`database_name`),
+  FOREIGN KEY `hetu_dbs_ibfk_1` (`catalog_name`) REFERENCES `hetu_ctlgs` (`catalog_name`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_tbls` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `database_id` bigint(20) NOT NULL,
+  `table_name` varchar(256) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  `view_original_text` text DEFAULT NULL,
+  `create_time` bigint(20) NOT NULL,
+  `owner` varchar(767) DEFAULT NULL,
+  `comment` varchar(4000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `database_id` (`database_id`,`table_name`),
+  FOREIGN KEY `hetu_tbls_ibfk_1` (`database_id`) REFERENCES `hetu_dbs` (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_tab_cols` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `table_id` bigint(20) NOT NULL,
+  `column_name` varchar(256) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  `comment` varchar(4000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `table_id` (`table_id`,`column_name`),
+  FOREIGN KEY `hetu_tab_cols_ibfk_1` (`table_id`) REFERENCES `hetu_tbls` (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_catalog_params` (
+  `catalog_id` bigint(20) NOT NULL,
+  `param_key` varchar(256) NOT NULL,
+  `param_value` text DEFAULT NULL,
+  PRIMARY KEY (`catalog_id`,`param_key`),
+  FOREIGN KEY `hetu_catalog_params_ibfk_1` (`catalog_id`) REFERENCES `hetu_ctlgs` (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_database_params` (
+  `database_id` bigint(20) NOT NULL,
+  `param_key` varchar(256) NOT NULL,
+  `param_value` text DEFAULT NULL,
+  PRIMARY KEY (`database_id`,`param_key`),
+  FOREIGN KEY `hetu_database_params_ibfk_1` (`database_id`) REFERENCES `hetu_dbs` (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_table_params` (
+  `table_id` bigint(20) NOT NULL,
+  `param_key` varchar(256) NOT NULL,
+  `param_value` text DEFAULT NULL,
+  PRIMARY KEY (`table_id`,`param_key`),
+  FOREIGN KEY `hetu_table_params_ibfk_1` (`table_id`) REFERENCES `hetu_tbls` (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_column_params` (
+  `column_id` bigint(20) NOT NULL,
+  `param_key` varchar(256) NOT NULL,
+  `param_value` text DEFAULT NULL,
+  PRIMARY KEY (`column_id`,`param_key`),
+  FOREIGN KEY `hetu_column_params_ibfk_1` (`column_id`) REFERENCES `hetu_tab_cols` (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_tab_lock` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `resource` int(11) NOT NULL,
+  `description` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `resource` (`resource`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_favorite` (
+  `creationTime` datetime DEFAULT current_timestamp(),
+  `user` varchar(20) NOT NULL,
+  `query` varchar(600) NOT NULL,
+  `catalog` varchar(30) NOT NULL,
+  `schemata` varchar(30) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `favorite_uni` (`user`,`query`,`catalog`,`schemata`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `hetu_query_history` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user` varchar(20) NOT NULL,
+  `source` varchar(30) NOT NULL,
+  `queryId` varchar(50) NOT NULL,
+  `resource` varchar(30) NOT NULL,
+  `query` varchar(10000) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `failed` varchar(30) DEFAULT NULL,
+  `createTime` varchar(30) DEFAULT NULL,
+  `elapsedTime` varchar(30) NOT NULL,
+  `cpuTime` varchar(30) NOT NULL,
+  `executionTime` varchar(30) NOT NULL,
+  `catalog` varchar(30) NOT NULL,
+  `schemata` varchar(30) NOT NULL,
+  `currentMemory` varchar(30) NOT NULL,
+  `cumulativeUserMemory` double NOT NULL,
+  `jsonString` text NOT NULL,
+  `completedDrivers` int(11) NOT NULL,
+  `runningDrivers` int(11) NOT NULL,
+  `queuedDrivers` int(11) NOT NULL,
+  `totalCpuTime` varchar(50) NOT NULL,
+  `totalMemoryReservation` varchar(50) NOT NULL,
+  `peakTotalMemoryReservation` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `query_history_queryId_uindex` (`queryId`)
+);
